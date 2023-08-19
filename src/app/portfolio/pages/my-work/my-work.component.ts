f4c880d257137ee635a364f3ca9087aea1f9da7f
JuanPaAvalos <juanpablordz0524@gmail.com>
2023-08-19 16:32:56 -0500

    fix: create Project interface file

--- src/app/portfolio/pages/my-work/my-work.component.ts
+++ src/app/portfolio/pages/my-work/my-work.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Project } from '../../interface/project.interface';
+import { Project } from '../../interfaces/project.interface';
 
 @Component({
   selector: 'app-my-work',
