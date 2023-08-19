fef4fadff1a201cdd5a71860524b573f22b6838e
JuanPaAvalos <juanpablordz0524@gmail.com>
2023-08-19 13:11:28 -0500

    feat: add skills section

--- src/app/portfolio/components/skills/skills.component.ts
+++ src/app/portfolio/components/skills/skills.component.ts
@@ -3,8 +3,45 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent {
+  public skills: skill[] = [
+    {
+      name: 'Angular',
+      img: 'assets/icons/skills/angular.png',
+      description: 'Lorem Ipsum is simply dummy text.',
+    },
+    {
+      name: 'Angular',
+      img: 'assets/icons/skills/angular.png',
+      description: 'Lorem Ipsum is simply dummy text.',
+    },
+    {
+      name: 'Angular',
+      img: 'assets/icons/skills/angular.png',
+      description: 'Lorem Ipsum is simply dummy text.',
+    },
+    {
+      name: 'Angular',
+      img: 'assets/icons/skills/angular.png',
+      description: 'Lorem Ipsum is simply dummy text.',
+    },
+    {
+      name: 'Angular',
+      img: 'assets/icons/skills/angular.png',
+      description: 'Lorem Ipsum is simply dummy text.',
+    },
+    {
+      name: 'Angular',
+      img: 'assets/icons/skills/angular.png',
+      description: 'Lorem Ipsum is simply dummy text.',
+    },
+  ];
+}
 
+interface skill {
+  name: string;
+  img: string;
+  description: string;
 }
