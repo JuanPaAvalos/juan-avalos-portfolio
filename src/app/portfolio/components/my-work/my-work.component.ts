1df22ab52ddcabd6341e4c502afd61d2b1e242b8
JuanPaAvalos <juanpablordz0524@gmail.com>
2023-08-19 16:18:26 -0500

    feat: add my work section

--- src/app/portfolio/components/my-work/my-work.component.ts
+++ src/app/portfolio/components/my-work/my-work.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Project } from '../../interface/project.interface';
 
 @Component({
   selector: 'app-my-work',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-work.component.scss']
 })
 export class MyWorkComponent {
-
+  public projects: Project[] = [
+    {
+      name: 'gifs app',
+      siteLink: 'https://juanpaavalos.github.io/GifsApp/',
+      img: 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2018/08/one-piece_0.jpg?tf=1200x',
+      description: 'Apliación para poder visualizar gifs utilizando peticiones http a un servicio de gifs y utilizando fuciones como local storage y servicios de angular',
+      githubLink: 'https://github.com/JuanPaAvalos/GifsApp'
+    },
+    {
+      name: 'gifs app',
+      siteLink: 'https://juanpaavalos.github.io/GifsApp/',
+      img: 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2018/08/one-piece_0.jpg?tf=1200x',
+      description: 'Apliación para poder visualizar gifs utilizando peticiones http a un servicio de gifs y utilizando fuciones como local storage y servicios de angular',
+      githubLink: 'https://github.com/JuanPaAvalos/GifsApp'
+    },
+    {
+      name: 'gifs app',
+      siteLink: 'https://juanpaavalos.github.io/GifsApp/',
+      img: 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2018/08/one-piece_0.jpg?tf=1200x',
+      description: 'Apliación para poder visualizar gifs utilizando peticiones http a un servicio de gifs y utilizando fuciones como local storage y servicios de angular',
+      githubLink: 'https://github.com/JuanPaAvalos/GifsApp'
+    },
+    {
+      name: 'gifs app',
+      siteLink: 'https://juanpaavalos.github.io/GifsApp/',
+      img: 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2018/08/one-piece_0.jpg?tf=1200x',
+      description: 'Apliación para poder visualizar gifs utilizando peticiones http a un servicio de gifs y utilizando fuciones como local storage y servicios de angular',
+      githubLink: 'https://github.com/JuanPaAvalos/GifsApp'
+    }
+  ];
 }
+
