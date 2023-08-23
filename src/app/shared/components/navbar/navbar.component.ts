75febe256200077f68ca300cf69a48717171191c
JuanPaAvalos <juanpablordz0524@gmail.com>
2023-08-23 09:07:48 -0500

    fix: update navbar sections names

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -9,10 +9,10 @@ export class NavbarComponent {
   public showMenu: boolean = false;
 
   public siteLinks = [
-    { name: 'portfolio', route: 'portfolio/home' },
-    { name: 'about-me', route: 'portfolio/about-me' },
-    { name: 'experience', route: 'portfolio/experience' },
-    { name: 'skills', route: 'portfolio/skills' },
-    { name: 'my-work', route: 'portfolio/my-work' },
+    { name: 'Inicio', route: 'portfolio/home' },
+    { name: 'Sobre Mi', route: 'portfolio/about-me' },
+    { name: 'Experiencia', route: 'portfolio/experience' },
+    { name: 'Habilidades', route: 'portfolio/skills' },
+    { name: 'Portafolio', route: 'portfolio/my-work' },
   ];
 }
