487db954ff842a41c6718a3399b815e5ec0d1a55
JuanPaAvalos <juanpablordz0524@gmail.com>
2024-04-04 08:16:44 -0500

    feat: add navbar color change on scroll

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -15,4 +16,20 @@ export class NavbarComponent {
     { name: 'Habilidades', route: 'portfolio/skills' },
     { name: 'Portafolio', route: 'portfolio/my-work' },
   ];
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Asegúrate de que estás ejecutando esto en el navegador
+    if (isPlatformBrowser(this.platformId)) {
+      const element = document.querySelector('.navbar-desktop');
+      if (window.pageYOffset > 50) { // Ajusta el 100 según sea necesario
+        element!.classList.add('navbar-desktop-dark');
+      } else {
+        element!.classList.remove('navbar-desktop-dark');
+      }
+    }
+  }
+
 }
